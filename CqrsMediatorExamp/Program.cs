cf78ca338c9a011b2badb12a5902c0044eff230c
Amir Delfan <amirdelfan@delfanit.onmicrosoft.com>
2022-01-23 20:11:23 +0100

    code clean

--- CqrsMediatorExamp/Program.cs
+++ CqrsMediatorExamp/Program.cs
@@ -1,8 +1,24 @@
+using CqrsMediatorExamp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using CqrsMediatorExamp.Domain.Commands.Users;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("CQRS_SAMPLE");
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddMediatR(typeof(CreateUserCommand).GetTypeInfo().Assembly);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

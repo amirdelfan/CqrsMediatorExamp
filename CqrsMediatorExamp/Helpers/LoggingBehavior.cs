29b1259262d5737da2bfc9325afe20b20206866a
Amir Delfan <amirdelfan@delfanit.onmicrosoft.com>
2022-01-23 20:41:43 +0100

    added logging

---
+++ CqrsMediatorExamp/Helpers/LoggingBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using MediatR;
+
+namespace CqrsMediatorExamp.Helpers
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+         where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var requestName = request.GetType().Name;
+            var requestGuid = Guid.NewGuid().ToString();
+
+            var requestNameWithGuid = $"{requestName} [{requestGuid}]";
+
+            string? currentUser = _httpContextAccessor?.HttpContext?.User?.Identity?.Name;
+            if (!string.IsNullOrEmpty(currentUser))
+            {
+                _logger.LogInformation($"{requestNameWithGuid} [{currentUser}]");
+            }
+            else
+            {
+                _logger.LogInformation($"{requestNameWithGuid}");
+            }
+
+            TResponse response;
+
+            try
+            {
+                try
+                {
+                    _logger.LogDebug($"[PROPS] {requestNameWithGuid} {JsonSerializer.Serialize(request)}");
+                }
+                catch (NotSupportedException e)
+                {
+                    _logger.LogError($"[Serialization ERROR] {requestNameWithGuid} Could not serialize the request.", e);
+                }
+
+                response = await next();
+            }
+            finally
+            {
+                stopwatch.Stop();
+                _logger.LogDebug(
+                    $"[END] {requestNameWithGuid}; Execution time={stopwatch.ElapsedMilliseconds}ms");
+            }
+
+            return response;
+        }
+    }
+}
--- CqrsMediatorExamp/Program.cs
+++ CqrsMediatorExamp/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using MediatR;
 using CqrsMediatorExamp.Domain.Commands.Users;
 using System.Reflection;
+using CqrsMediatorExamp.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +16,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("CQRS_SAMPLE");
 });
 
+// Dependecy injection 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Initialization of Commands and Queries in assemblies
 builder.Services.AddMediatR(typeof(CreateUserCommand).GetTypeInfo().Assembly);
+
+// Add logging
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+builder.Services.AddHttpContextAccessor();
+
+builder.Logging.AddEventLog(eventLogSettings =>
+{
+    eventLogSettings.SourceName = "CqrsMediatorExamp";
+    eventLogSettings.LogName = "CqrsMediatorExamp";
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

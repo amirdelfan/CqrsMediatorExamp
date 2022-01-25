a6459d677a9c36d1a3927d675189e9ae10c5f33d
Amir Delfan <amirdelfan@delfanit.onmicrosoft.com>
2022-01-25 13:09:20 +0100

    added unit-tests

---
+++ CqrsMediatorExamp.Tests/Queries/GetUserQueryTests.cs
@@ -0,0 +1,39 @@
+using CqrsMediatorExamp.Domain.Models;
+using CqrsMediatorExamp.Domain.Queries.Users;
+using CqrsMediatorExamp.Domain.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CqrsMediatorExamp.Tests.Queries
+{
+    public class GetUserQueryTests
+    {
+        private Mock<IUserRepository> mockUserRepository;
+
+        IList<User> users = new List<User>
+                {
+                    new User { Id = 1, Name = "John", Age = 22},
+                    new User { Id = 2, Name = "Olfa", Age = 30},
+                    new User { Id = 3, Name = "Anke", Age = 19}
+                };
+
+        public GetUserQueryTests()
+        {
+            mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository.Setup(mr => mr.FindByIdAsync(
+                It.IsAny<int>())).ReturnsAsync((int i) => users.Where(
+                x => x.Id == i).Single());
+        }
+
+        [Fact]
+        public async Task GetExistingUserAsync()
+        {
+            var handler = new GetUserQueryHandler(this.mockUserRepository.Object);
+            User result = await handler.Handle(new GetUserQuery(1), new System.Threading.CancellationToken());
+            Assert.NotNull(result);
+        }
+    }
+}
--- CqrsMediatorExamp/Domain/Queries/Users/GetUserQueryHandler.cs
+++ CqrsMediatorExamp/Domain/Queries/Users/GetUserQueryHandler.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using CqrsMediatorExamp.Domain.Models;
 using CqrsMediatorExamp.Domain.Repositories;
 using MediatR;

9291d37d8f61f3b50ba20d91c0a676267fa9cc92
Amir Delfan <amirdelfan@delfanit.onmicrosoft.com>
2022-01-25 13:46:12 +0100

    fluent validation added

---
+++ CqrsMediatorExamp/Domain/Validation/CreateUserCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using CqrsMediatorExamp.Domain.Commands.Users;
+using FluentValidation;
+
+namespace CqrsMediatorExamp.Domain.Validation
+{
+    public sealed class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).MinimumLength(3);
+        }
+    }
+}
